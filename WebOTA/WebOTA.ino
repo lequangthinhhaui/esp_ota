867d8bd3b0dedc54bc8fd34a644649b14248c9ac
enma <lequangthinhhaui@gmail.com>
2023-02-22 14:11:31 +0700

    add smartconfig esp-idf and create class in ota

--- WebOTA/WebOTA.ino
+++ WebOTA/WebOTA.ino
@@ -9,51 +9,72 @@
 #include <HTTPClient.h>
 #include <Update.h>
 
-#define HOST "http://192.168.5.236:8000/"
+#define HOST "http://192.168.2.191:8000/"
 
 HTTPClient client;  
 // Your WiFi credentials
-const char* ssid = "MTE_DEVELOP";
-const char* password = "meiko@12345mkac";
+const char* ssid = "MKAC";
+const char* password = "MKAC12345";
 // Global variables
-int totalLength;       //total size of firmware
-int currentLength = 0; //current size of written firmware
 
-void updating()
+
+
+class esp32Update
 {
-  client.begin(HOST);
-  int resp = client.GET();
-  Serial.print("Response: ");
-  Serial.println(resp);
-  if(resp == 200)
+  public:
+  int totalLength;       //total size of firmware
+  int currentLength = 0; //current size of written firmware
+  
+  void updateFirmware(uint8_t *data, size_t len){
+    Update.write(data, len);
+    currentLength += len;
+    // Print dots while waiting for update to finish
+    Serial.print('.');
+    // if current length of written firmware is not equal to total firmware size, repeat
+    if(currentLength != totalLength) return;
+    Update.end(true);
+    Serial.printf("\nUpdate Success, Total Size: %u\nRebooting...\n", currentLength);
+    // Restart ESP32 to see changes 
+    ESP.restart();
+  }  
+  void updating()
   {
-      totalLength = client.getSize();      // get length of document (is -1 when Server sends no Content-Length header)
-      int len = totalLength;      // transfer to local variable
-      Update.begin(UPDATE_SIZE_UNKNOWN);      // this is required to start firmware update process
-      Serial.printf("FW Size: %u\n",totalLength);
-      uint8_t buff[128] = { 0 };      // create buffer for read
-      WiFiClient * stream = client.getStreamPtr();      // get tcp stream
-      Serial.println("Updating firmware...");      // read all data from server
-      while(client.connected() && (len > 0 || len == -1))
-      {
-           size_t size = stream->available(); // get available data size
-           if(size) 
-           {
-              int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));// read up to 128 byte
-              updateFirmware(buff, c); // pass to function
-              if(len > 0) {
-                 len -= c;
-              }
-           }
-           delay(1);
-      }
-  }
-  else
-  {
-    Serial.println("Cannot download firmware file. Only HTTP response 200: OK is supported. Double check firmware location #defined in HOST.");
-  }
-  client.end(); 
-}
+    client.begin(HOST);
+    int resp = client.GET();
+    Serial.print("Response: ");
+    Serial.println(resp);
+    if(resp == 200)
+    {
+        totalLength = client.getSize();      // get length of document (is -1 when Server sends no Content-Length header)
+        int len = totalLength;      // transfer to local variable
+        Update.begin(UPDATE_SIZE_UNKNOWN);      // this is required to start firmware update process
+        Serial.printf("FW Size: %u\n",totalLength);
+        uint8_t buff[128] = { 0 };      // create buffer for read
+        WiFiClient * stream = client.getStreamPtr();      // get tcp stream
+        Serial.println("Updating firmware...");      // read all data from server
+        while(client.connected() && (len > 0 || len == -1))
+        {
+             size_t size = stream->available(); // get available data size
+             if(size) 
+             {
+                int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));// read up to 128 byte
+                updateFirmware(buff, c); // pass to function
+                if(len > 0) {
+                   len -= c;
+                }
+             }
+             delay(1);
+        }
+    }
+    else
+    {
+      Serial.println("Cannot download firmware file. Only HTTP response 200: OK is supported. Double check firmware location #defined in HOST.");
+    }
+    client.end(); 
+  }    
+};
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -73,9 +94,12 @@ void setup() {
 
 char buf[7];
 String data_receive;
+
+esp32Update esp32_update;
 void loop() 
 {
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) 
+  {
     // read the incoming bytes:
     int rlen = Serial.readBytes(buf, 7);
 
@@ -87,36 +111,17 @@ void loop()
     {
       Serial.print(buf[i]);      
     }
-//    Serial.println();
-    String my_string(buf); 
-    Serial.println(my_string);
     if(my_string == "up_date")
     {
-        updating();
+        esp32_update.updating();
     }
     else
     {
       Serial.println("no update");
     }
   }
-
-
 }
 
 // Function to update firmware incrementally
 // Buffer is declared to be 128 so chunks of 128 bytes
 // from firmware is written to device until server closes
-
-
-void updateFirmware(uint8_t *data, size_t len){
-  Update.write(data, len);
-  currentLength += len;
-  // Print dots while waiting for update to finish
-  Serial.print('.');
-  // if current length of written firmware is not equal to total firmware size, repeat
-  if(currentLength != totalLength) return;
-  Update.end(true);
-  Serial.printf("\nUpdate Success, Total Size: %u\nRebooting...\n", currentLength);
-  // Restart ESP32 to see changes 
-  ESP.restart();
-}
